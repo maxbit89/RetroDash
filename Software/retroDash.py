2670714a25088dee831cd0ea9ccafa9f8638e415
maxbit89@gmail.com <maxbit89@gmail.com>
2019-04-29 00:03:30 +0100

    added some menue entrys

--- Software/retroDash.py
+++ Software/retroDash.py
@@ -14,11 +14,18 @@ disp.begin()
 disp.clear()
 disp.display()
 
-def printd(text):
+MENUE_NETWORK = 0
+MENUE_RADIO   = 1
+MENUE_BLUETOOTH_MAIN = 2
+MENUE_BLUETOOTH_PAIR = 3
+
+menue_selection = MENUE_RADIO
+
+def printd(text, pos=(0,0)):
     image = Image.new('1', (disp.width, disp.height));
     font = ImageFont.load_default()
     draw = ImageDraw.Draw(image)
-    draw.text((0,0),text, font=font, fill=255)
+    draw.text(pos,text, font=font, fill=255)
     disp.image(image)
     disp.display()
 
@@ -33,4 +40,18 @@ def getOwnIp():
     finally:
         s.close()
     return ip
-printd("IP: %s" % (getOwnIp()))
+
+while 1:
+    if menue_selection ==  MENUE_NETWORK:
+        disp.clear()
+        printd("IP: %s" % (getOwnIp()))
+    elif menue_selection == MENUE_RADIO:
+        disp.clear()
+        printd("Radio: %3.2f MHz" % (100.2))
+    elif menue_selection == MENUE_BLUETOOTH_MAIN:
+        disp.clear()
+        printd("Bluetooth")
+    elif menue_selection == MENUE_BLUETOOTH_PAIR:
+        disp.clear()
+        printd("Bluetooth Pairing...")
+    time.sleep(0.2)
